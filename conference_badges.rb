65f74fc440adc4cbe280476b3e1e2a7a00997c6a
Megan Christine McClure <mcclme02@luther.edu>
2020-03-12 18:36:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -54,6 +54,11 @@ def assign_rooms(attendees)
   end 
 end 
 
-def printer()
-  
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end 
+  assign_rooms(attendees).each do |room|
+    puts room
+  end 
 end 
