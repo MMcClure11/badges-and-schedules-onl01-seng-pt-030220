49baf46a68f2f216f4d9ef932b717a8f5ea82e4d
Megan Christine McClure <mcclme02@luther.edu>
2020-03-12 18:33:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -53,3 +53,8 @@ def assign_rooms(attendees)
   return "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end 
 end 
+def assign_rooms(attendees)
+#   attendees.each_with_index.collect do |attendee, index|
+#     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+#   end
+# end
